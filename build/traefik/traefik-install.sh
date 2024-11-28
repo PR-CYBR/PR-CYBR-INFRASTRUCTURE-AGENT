54b64af3c51495b9fdae9513c4203585cea46135
Kylo P <100837335+cywf@users.noreply.github.com>
2024-11-28 17:17:05 -0400

    made comments in install script

--- build/traefik/traefik-install.sh
+++ build/traefik/traefik-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# ------ #
+# TODO #
+# - [ ] apply logic to have it open new shell / subshell
+# - [ ] check system for docker installation / version (if none found, install docker and dependaceies)
+# ------ #
+
 echo "Building and starting Traefik service..."
 docker-compose up --build -d
 echo "Traefik service is up and running." 
